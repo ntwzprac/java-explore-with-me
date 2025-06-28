a2e3721b1372b551d047dafcc8ee3c6ab36c1b83
ntwzprac <notweuz@yandex.ru>
2025-06-28 18:47:18 +0400

    Remove unused import

--- ewm-stats-service/ewm-stats-service/src/main/java/ru/practicum/statsservice/service/StatsServiceImpl.java
+++ ewm-stats-service/ewm-stats-service/src/main/java/ru/practicum/statsservice/service/StatsServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.practicum.statsservice.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
