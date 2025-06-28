2479737eac5216b78b0e7874d4f1fec4c2da6d1a
notweuz <weuz.main@gmail.com>
2025-06-28 17:18:51 +0400

    Move stats logic from controller to service & remove exceptions handling in StatsController

--- ewm-stats-service/ewm-stats-service/src/main/java/ru/practicum/statsservice/controller/StatsController.java
+++ ewm-stats-service/ewm-stats-service/src/main/java/ru/practicum/statsservice/controller/StatsController.java
@@ -1,61 +1,38 @@
 package ru.practicum.statsservice.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.statsdto.EndpointHit;
-import ru.practicum.statsdto.EndpointHitResponse;
-import ru.practicum.statsdto.ErrorResponse;
 import ru.practicum.statsdto.ViewStats;
 import ru.practicum.statsservice.service.StatsService;
 
 import jakarta.validation.Valid;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 public class StatsController {
     private final StatsService statsService;
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Autowired
+    public StatsController(StatsService statsService) {
+        this.statsService = statsService;
+    }
 
     @PostMapping("/hit")
-    public ResponseEntity<EndpointHitResponse> hit(@Valid @RequestBody EndpointHit hit) {
-        EndpointHitResponse response = statsService.saveHit(hit);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    public ResponseEntity<Void> hit(@Valid @RequestBody EndpointHit hit) {
+        statsService.handleHit(hit);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/stats")
     public ResponseEntity<List<ViewStats>> getStats(
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
+            @RequestParam String start,
+            @RequestParam String end,
             @RequestParam(required = false) List<String> uris,
             @RequestParam(defaultValue = "false") boolean unique) {
-
-        List<ViewStats> stats = statsService.getStats(start, end, uris, unique);
+        List<ViewStats> stats = statsService.handleGetStats(start, end, uris, unique);
         return ResponseEntity.ok(stats);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
-        ErrorResponse error = ErrorResponse.builder()
-                .message(e.getMessage())
-                .timestamp(LocalDateTime.now().format(FORMATTER))
-                .status(HttpStatus.BAD_REQUEST.value())
-                .build();
-        return ResponseEntity.badRequest().body(error);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
-        ErrorResponse error = ErrorResponse.builder()
-                .message(e.getMessage())
-                .timestamp(LocalDateTime.now().format(FORMATTER))
-                .status(HttpStatus.BAD_REQUEST.value())
-                .build();
-        return ResponseEntity.badRequest().body(error);
     }
 }
--- ewm-stats-service/ewm-stats-service/src/main/java/ru/practicum/statsservice/service/StatsService.java
+++ ewm-stats-service/ewm-stats-service/src/main/java/ru/practicum/statsservice/service/StatsService.java
@@ -1,7 +1,6 @@
 package ru.practicum.statsservice.service;
 
 import ru.practicum.statsdto.EndpointHit;
-import ru.practicum.statsdto.EndpointHitResponse;
 import ru.practicum.statsdto.ViewStats;
 
 import java.time.LocalDateTime;
@@ -9,11 +8,15 @@ import java.util.List;
 
 public interface StatsService {
 
-    EndpointHitResponse saveHit(EndpointHit hit);
+    void saveHit(EndpointHit hit);
 
     List<ViewStats> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, boolean unique);
 
     void validateDateRange(LocalDateTime start, LocalDateTime end);
 
     List<String> decodeUris(List<String> uris);
+
+    void handleHit(EndpointHit hit);
+
+    List<ViewStats> handleGetStats(String start, String end, List<String> uris, boolean unique);
 }
