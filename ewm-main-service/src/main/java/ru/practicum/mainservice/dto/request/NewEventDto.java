cd05c6cb7af65ced7e34c03f5763c6afaee48a14
ntwzprac <notweuz@yandex.ru>
2025-07-01 20:38:11 +0400

    Add positive validation

--- ewm-main-service/src/main/java/ru/practicum/mainservice/dto/request/NewEventDto.java
+++ ewm-main-service/src/main/java/ru/practicum/mainservice/dto/request/NewEventDto.java
@@ -2,6 +2,7 @@ package ru.practicum.mainservice.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,6 +38,7 @@ public class NewEventDto {
 
     private Boolean paid = false;
 
+    @Positive
     private Integer participantLimit = 0;
 
     private Boolean requestModeration = true;
