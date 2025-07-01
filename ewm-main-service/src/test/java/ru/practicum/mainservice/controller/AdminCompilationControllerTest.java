b8f1c5948b780e21d7e95a328ddfe49eba367c3b
ntwzprac <notweuz@yandex.ru>
2025-07-01 17:34:50 +0400

    Add more tests

---
+++ ewm-main-service/src/test/java/ru/practicum/mainservice/controller/AdminCompilationControllerTest.java
@@ -0,0 +1,58 @@
+package ru.practicum.mainservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.mainservice.dto.request.NewCompilationDto;
+import ru.practicum.mainservice.dto.request.UpdateCompilationRequest;
+import ru.practicum.mainservice.dto.response.CompilationDto;
+import ru.practicum.mainservice.service.CompilationService;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AdminCompilationController.class)
+class AdminCompilationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private CompilationService compilationService;
+
+    @Test
+    void saveCompilation_success() throws Exception {
+        NewCompilationDto request = new NewCompilationDto();
+        request.setTitle("Test Compilation");
+        CompilationDto response = new CompilationDto();
+        Mockito.when(compilationService.saveCompilation(any())).thenReturn(response);
+        mockMvc.perform(post("/admin/compilations")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void deleteCompilation_success() throws Exception {
+        mockMvc.perform(delete("/admin/compilations/1"))
+                .andExpect(status().isNoContent());
+        Mockito.verify(compilationService).deleteCompilation(1L);
+    }
+
+    @Test
+    void updateCompilation_success() throws Exception {
+        UpdateCompilationRequest request = new UpdateCompilationRequest();
+        CompilationDto response = new CompilationDto();
+        Mockito.when(compilationService.updateCompilation(any(), any())).thenReturn(response);
+        mockMvc.perform(patch("/admin/compilations/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+    }
+} 
---
+++ ewm-main-service/src/test/java/ru/practicum/mainservice/controller/AdminUserControllerTest.java
@@ -0,0 +1,61 @@
+package ru.practicum.mainservice.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.mainservice.dto.request.NewUserRequest;
+import ru.practicum.mainservice.dto.response.UserDto;
+import ru.practicum.mainservice.service.UserService;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AdminUserController.class)
+class AdminUserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private UserService userService;
+
+    @Test
+    void registerUser_success() throws Exception {
+        NewUserRequest request = new NewUserRequest();
+        request.setName("Test User");
+        request.setEmail("test@example.com");
+        UserDto response = new UserDto();
+        Mockito.when(userService.registerUser(any())).thenReturn(response);
+
+        mockMvc.perform(post("/admin/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void deleteUser_success() throws Exception {
+        mockMvc.perform(delete("/admin/users/1"))
+                .andExpect(status().isNoContent());
+        Mockito.verify(userService).deleteUser(1L);
+    }
+
+    @Test
+    void getUsers_success() throws Exception {
+        Mockito.when(userService.getUsers(any(), anyInt(), anyInt())).thenReturn(Collections.emptyList());
+        mockMvc.perform(get("/admin/users"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+} 
