f63fb5286897ef045b10791bb9f3966ef02e29db
ntwzprac <notweuz@yandex.ru>
2025-07-01 16:15:34 +0400

    Change to lombok usage

--- ewm-main-service/src/main/java/ru/practicum/mainservice/dto/mapper/CompilationMapper.java
+++ ewm-main-service/src/main/java/ru/practicum/mainservice/dto/mapper/CompilationMapper.java
@@ -1,28 +1,27 @@
 package ru.practicum.mainservice.dto.mapper;
 
+import lombok.Setter;
 import ru.practicum.mainservice.dto.request.NewCompilationDto;
 import ru.practicum.mainservice.dto.request.UpdateCompilationRequest;
 import ru.practicum.mainservice.dto.response.CompilationDto;
-import ru.practicum.mainservice.model.Compilation;
 import ru.practicum.mainservice.dto.response.EventShortDto;
-import ru.practicum.mainservice.dto.mapper.EventMapper;
+import ru.practicum.mainservice.model.Compilation;
 import ru.practicum.mainservice.repository.EventRepository;
+
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CompilationMapper {
+    @Setter
     private static EventRepository eventRepository;
-    public static void setEventRepository(EventRepository repository) {
-        eventRepository = repository;
-    }
 
     public static CompilationDto toDto(Compilation compilation) {
         if (compilation == null) return null;
         Set<EventShortDto> events = compilation.getEvents() == null ? null :
-            compilation.getEvents().stream()
-                .map(eventId -> eventRepository.findById(eventId).map(EventMapper::toShortDto).orElse(null))
-                .filter(e -> e != null)
-                .collect(Collectors.toSet());
+                compilation.getEvents().stream()
+                        .map(eventId -> eventRepository.findById(eventId).map(EventMapper::toShortDto).orElse(null))
+                        .filter(e -> e != null)
+                        .collect(Collectors.toSet());
         return CompilationDto.builder()
                 .id(compilation.getId())
                 .title(compilation.getTitle())
